1a8bce3125040e2173e36ed051e10afe081f8741
Nazhat Afza Zain <nazhatafza1@gmail.com>
2021-09-10 07:39:36 -0700

    push aja

---
+++ Nijat/Assets/Scripts/CoinRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+
+    public float RotationSpeed;
+
+
+
+    void FixedUpdate()
+    {
+
+        gameObject.transform.RotateAround(gameObject.transform.position, transform.up, RotationSpeed);
+
+    }
+}
